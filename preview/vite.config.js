4962762f4c59b060676bb47850398aa4ee2718be
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-13 11:58:27 +0100

    fix vite exports

--- preview/vite.config.js
+++ preview/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 // import { MDZ } from "../src/vite/index.js";
-import ViteMDZ  from "../src/bundlers/vite.js"
+// import ViteMDZ  from "../src/bundlers/vite.js"
+import ViteMDZ from "mdzjs/vite"
+
 export default defineConfig({
     plugins : [ViteMDZ()]
 })
